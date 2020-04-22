8b225dbf3db2a10891ca528ede831b60f52e2d52
BuildTools <unconfigured@null.spigotmc.org>
2020-04-22 17:01:04 +0200

    added genres

--- template/genres.php
+++ template/genres.php
@@ -1,10 +1,33 @@
-<div>
-    <i></i>
-    <i></i>
-    <i></i>
-    <i></i>
-    <i></i>
-    <i></i>
-    <i></i>
-    <i></i>
+<div class="genres row">
+    <div class="col l1 offset-l2">
+        <i class="fas fa-swords"></i>
+        <p>Action</p>
+    </div>
+    <div class="col l1">
+        <i class="fas fa-camera"></i>
+        <p>Documentary</p>
+    </div>
+    <div class="col l1"><i class="fas fa-ghost"></i>
+        <p>Horror</p>
+    </div>
+    <div class="col l1">
+        <i class="fas fa-grin-squint-tears"></i>
+        <p>Comedy</p>
+    </div>
+    <div class="col l1">
+        <i class="fas fa-siren-on"></i>
+        <p>Crime</p>
+    </div>
+    <div class="col l1">
+        <i class="fas fa-heart"></i>
+        <p>Romance</p>
+    </div>
+    <div class="col l1">
+        <i class="fas fa-alien"></i>
+        <p>Sci fi & fantasy</p>
+    </div>
+    <div class="col l1">
+        <i class="fas fa-skull"></i>
+        <p>Thriller</p>
+    </div>
 </div>
